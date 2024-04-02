dbcbbad5b823f89d66e023b60cb68e28291ed3b9
Shashwat-Choudhari <sc.21u10640@btech.nitdgp.ac.in>
2024-04-02 12:27:12 +0530

    add user login

--- index.js
+++ index.js
@@ -115,6 +115,72 @@ app.get("/reviews", async(req,res)=>{
     }
 });
 
+app.get("/login", (req, res) => {
+    res.render("login.ejs");
+  });
+  
+  app.get("/register", (req, res) => {
+    res.render("register.ejs");
+  });
+  
+  app.post("/register", async (req, res) => {
+    const email = req.body.username;
+    const password = req.body.password;
+  
+    try {
+      const checkResult = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+  
+      if (checkResult.rows.length > 0) {
+        res.send("Email already exists. Try logging in.");
+      } else {
+        const result = await db.query(
+          "INSERT INTO users (email, password) VALUES ($1, $2)",
+          [email, password]
+        );
+        try {
+            const items = await allItems();
+            res.render("menu.ejs",{items: items});
+        } catch (error) {
+            console.log(error);        
+        }
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+  
+  app.post("/login", async (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+  
+    try {
+      const result = await db.query("SELECT * FROM users WHERE email = $1", [
+        email,
+      ]);
+      if (result.rows.length > 0) {
+        const user = result.rows[0];
+        const storedPassword = user.password;
+  
+        if (password === storedPassword) {
+            try {
+                const items = await allItems();
+                res.render("menu.ejs",{items: items});
+            } catch (error) {
+                console.log(error);        
+            }
+        } else {
+          res.send("Incorrect Password");
+        }
+      } else {
+        res.send("User not found");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
 app.listen(port,()=>{
     console.log("Listening on port " + port);
 });
